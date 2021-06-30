e8849b96455b704c965029d633c81077d27190c0
arnaud76-max <72136722+arnaud76-max@users.noreply.github.com>
2021-06-30 10:23:38 +0200

    Update App.js

--- cinezing/src/App.js
+++ cinezing/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
-import Movie from './components/Movie';
+import Movie from './components/Movies';
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
